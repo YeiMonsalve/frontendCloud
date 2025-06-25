99adaea178f9dc48d55673d2f1f34b5026e39a12
YeiMonsalve <168552590+YeiMonsalve@users.noreply.github.com>
2025-06-24 20:19:14 -0500

    Prueba para la nube

---
+++ remix.config.js
@@ -0,0 +1,9 @@
+export default {
+    future: {
+        v3_routeConvention: true
+    },
+    server: false, // ❗ App estática, sin SSR
+    appDirectory: "app",
+    assetsBuildDirectory: "build/client",
+    publicPath: "/build/",
+};
