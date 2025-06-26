597b563c6791572f2cc820cdc55384cfc3ff4cd2
Marinellys Figueroa <marinellys.figueroa@gmail.com>
2025-06-25 20:57:26 -0500

    cambio de nombre de paquete en root

--- app/root.tsx
+++ app/root.tsx
@@ -11,9 +11,9 @@ import { json, type LinksFunction } from "@remix-run/node";
 import { Toaster } from "react-hot-toast";
 import "./tailwind.css";
 import { checkAuth } from "~/services/authService";
-import Navbar from "~/Components/Navbar/Navbar";
-import Footer from "~/Components/Footer/Footer";
-import WompiButton from "~/Components/wompi";
+import Navbar from "~/components/Navbar/Navbar";
+import Footer from "~/components/Footer/Footer";
+import WompiButton from "~/components/wompi";
 
 
 export const loader = async ({ request }: { request: Request }) => {
